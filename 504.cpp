ab309d2c94fd7540b8a65ba5369227001a0c56b5
Ritika dubey <ritikadubey1305@gmail.com>
2026-03-22 00:31:36 +0530

    commit

---
+++ 504.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class A{
+    private:
+    string a;
+
+};
+
+class B{
+    private:
+    string ch;
+};
+
+
+int main()
+{
+    A s1;
+    B s2;
+
+    return 0;
+    
+}
